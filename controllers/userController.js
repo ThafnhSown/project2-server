949536a9ee23f2af09ca9d785ca72b3e18cfd272
ThafnhSown <sontnt204685@gmail.com>
2023-05-03 23:49:37 +0700

    first

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+//login user
+const loginUser = async(req, res) => {
+    res.json({mssg: 'Login user'})
+}
+
+//signup user
+const signupUser = async(req, res) => {
+    res.json({mssg: 'Signup user'})
+}
+
+
+module.exports = { signupUser, loginUser }
---
+++ temp/xi0z5re.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Log in thanfh cong";
+}
